fda586af22493e5a79cb077b1131598ea547ef0b
mario-neb <50882870+mario-neb@users.noreply.github.com>
2019-11-28 21:55:47 +0100

    Configured environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
 import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from "./app.component";
+import { AngularFirestoreModule} from '@angular/fire/firestore';
 import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-
+import { AngularFireModule } from '@angular/fire';
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { ComponentsModule } from "./components/components.module";
+import { environment } from 'src/environments/environment';
+import { SharedModule } from './shared/modules/shared.module';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
     FormsModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
     HttpClientModule,
     ComponentsModule,
+    SharedModule,
     NgbModule,
     RouterModule,
     AppRoutingModule,
@@ -27,6 +33,7 @@ import { ComponentsModule } from "./components/components.module";
   ],
   declarations: [AppComponent, AdminLayoutComponent, AuthLayoutComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
---
+++ src/app/shared/modules/shared.module.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from 'src/app/shared/modules/material.module';
+// import { NotFoundComponent } from '../components/not-found/not-found.component';
+// import { HeaderComponent } from '../components/header/header.component';
+// import { SidenavListComponent } from '../components/sidenav-list/sidenav-list.component';
+// import { LayoutComponent } from '../components/layout/layout.component';
+
+
+@NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  declarations:
+    [
+      // HeaderComponent,
+      // SidenavListComponent,
+      // NotFoundComponent,
+      // LayoutComponent
+    ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    RouterModule
+  ],
+  exports: [
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    RouterModule,
+    // HeaderComponent,
+    // SidenavListComponent,
+    // NotFoundComponent,
+    // LayoutComponent
+  ],
+})
+export class SharedModule { }
