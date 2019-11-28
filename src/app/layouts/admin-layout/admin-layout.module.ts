c856b55f6b567a5cc893abd3bd901855fc0e5c77
mario-neb <50882870+mario-neb@users.noreply.github.com>
2019-11-28 21:58:10 +0100

    configured routes

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -7,20 +7,22 @@ import { FormsModule } from "@angular/forms";
 import { AdminLayoutRoutes } from "./admin-layout.routing";
 import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
 import { IconsComponent } from "../../pages/icons/icons.component";
-import { MapComponent } from "../../pages/map/map.component";
 import { NotificationsComponent } from "../../pages/notifications/notifications.component";
 import { UserComponent } from "../../pages/user/user.component";
 import { TablesComponent } from "../../pages/tables/tables.component";
 import { TypographyComponent } from "../../pages/typography/typography.component";
-// import { RtlComponent } from "../../pages/rtl/rtl.component";
 
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { DisplayUserComponent } from 'src/app/pages/display-user/display-user.component';
+import { UserListComponent } from 'src/app/pages/user-list/user-list.component';
+import { SharedModule } from 'src/app/shared/modules/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(AdminLayoutRoutes),
     FormsModule,
+    SharedModule,
     HttpClientModule,
     NgbModule,
   ],
@@ -31,8 +33,8 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
     IconsComponent,
     TypographyComponent,
     NotificationsComponent,
-    MapComponent,
-    // RtlComponent
+    DisplayUserComponent,
+    UserListComponent
   ]
 })
 export class AdminLayoutModule {}
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -2,20 +2,21 @@ import { Routes } from "@angular/router";
 
 import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
 import { IconsComponent } from "../../pages/icons/icons.component";
-import { MapComponent } from "../../pages/map/map.component";
 import { NotificationsComponent } from "../../pages/notifications/notifications.component";
 import { UserComponent } from "../../pages/user/user.component";
 import { TablesComponent } from "../../pages/tables/tables.component";
 import { TypographyComponent } from "../../pages/typography/typography.component";
-// import { RtlComponent } from "../../pages/rtl/rtl.component";
+import { UserListComponent } from 'src/app/pages/user-list/user-list.component';
+import { DisplayUserComponent } from 'src/app/pages/display-user/display-user.component';
+
 
 export const AdminLayoutRoutes: Routes = [
   { path: "dashboard", component: DashboardComponent },
   { path: "icons", component: IconsComponent },
-  { path: "maps", component: MapComponent },
   { path: "notifications", component: NotificationsComponent },
   { path: "user", component: UserComponent },
   { path: "tables", component: TablesComponent },
   { path: "typography", component: TypographyComponent },
-  // { path: "rtl", component: RtlComponent }
+  {path: 'user-list',component: UserListComponent,pathMatch: 'full'},
+  {path: 'display-user/:id',pathMatch: 'full',component: DisplayUserComponent}
 ];
